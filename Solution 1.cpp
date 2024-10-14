87d054904b8078eee1d3b85210716cfe93977b46
justcigarette <vuphuonglinh2002bg@gmail.com>
2024-10-14 23:22:03 +0700

    add hello

--- Solution 1.cpp
+++ Solution 1.cpp
@@ -1,65 +1,40 @@
-#include<math.h>
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
+void sortColors(vector<string>& colors) {
+    int low = 0;           
+    int mid = 0;           
+    int high = colors.size() - 1;  
 
-
-int songuyento(int n) {
-	for (int i = 2; i <= sqrt(n); i++) {
-		if (n % i == 0) {
-			return 0;
-		}
-	}
-	return 1;
+    while (mid <= high) {
+        if (colors[mid] == "do") {
+            swap(colors[low], colors[mid]);
+            low++;
+            mid++;
+        } else if (colors[mid] == "trang") {
+            mid++;
+        } else if (colors[mid] == "xanh") {
+            swap(colors[mid], colors[high]);
+            high--;  
+        }
+    }
 }
 
-
-
-int soNUAnguyento(int n) {
-	for (int i = 2; i < n; i++) {
-		for (int j = 2; j < n; j++) {
-			if (songuyento(i) && songuyento(j) == 1) {
-				if (i * j == n) {
-					return 1;
-				}
-			}
-		}
-	}
-	return 0;
-}
 int main() {
-	int a[100], blumNumber[100];
-	int count = 0;
-	for (int i = 0;i < 100;i++) {
-		a[i] = i + 1;
-	}
-	for (int i = 0;i < 100;i++) {
-		if (soNUAnguyento(a[i]) == 1) {
-			blumNumber[count++] = a[i];
-		}
-	}
-	for (int i = 0;i < count;i++) {
-		for (int j = 0;j < count;j++) {
-			int sum = blumNumber[j] + blumNumber[i];
-			for (int k = 0;k < count;k++) {
-				if (sum == blumNumber[k]) { 
-					cout << "(" << blumNumber[i] << ", " << blumNumber[j] << ")" << endl;
-				}
-			}
-		}
-	}
-	int M;
-	bool check = false;
-	cout << "nhap M:";
-	cin >> M;
-	for (int i = 0;i < count;i++) {
-		if (M == blumNumber[i]) {
-			check = true;
-		}
-	}
-	if (check == true) {
-		cout << "M co ton tai trong day Blum" << endl;
-	}
-	else cout << "khong ton tai";
+
+    vector<string> colors = {"xanh", "do", "xanh", "do", "do", "trang", "do", "xanh", "trang"};
+    
+    sortColors(colors);
+    
+    cout << "Ket qua sau khi sap xep: ";
+    for (const string& color : colors) {
+        cout << color << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
 
